dcbb13681e0b31edc6045ad33d2c56fb1f4a07fd
Damien Jolly <DamienJolly@hotmail.com>
2019-01-28 23:36:26 +0000

    Changed entry point to async.

--- AliasPro/Program.cs
+++ AliasPro/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace AliasPro
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+		/// Boot up the application
+		/// </summary>
+        private async Task Boot()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing.. Hello World!");
+
+            while (true)
+            {
+                if (Console.ReadKey(false).Key == ConsoleKey.Enter)
+                {
+                    await DisposeAsync();
+                }
+            }
+        }
+
+        /// <summary>
+		/// Dispose off the application
+		/// </summary>
+        private async Task DisposeAsync()
+        {
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+		/// Entry point for the application
+		/// </summary>
+        public static async Task Main()
+        {
+            await new Program().Boot();
         }
     }
 }
