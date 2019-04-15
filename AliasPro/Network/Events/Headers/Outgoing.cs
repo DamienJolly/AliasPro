4133dfd9e0272898c371b5507f7494740c62e910
Damien Jolly <DamienJolly@hotmail.com>
2019-04-15 22:16:28 +0100

    Added update currency points composer.

--- AliasPro/Network/Events/Headers/Outgoing.cs
+++ AliasPro/Network/Events/Headers/Outgoing.cs
@@ -11,6 +11,7 @@
         internal const short UserDataMessageComposer = 3862;
         internal const short FurniListMessageComposer = 826;
         internal const short UserCreditsMessageComposer = 1232;
+        internal const short UserPointsMessageComposer = 1532;
         internal const short UserCurrencyMessageComposer = 2380;
         internal const short UserProfileMessageComposer = 1459;
         internal const short MeMenuSettingsMessageComposer = 2204;
---
+++ AliasPro/Players/Packets/Composers/UserPointsComposer.cs
@@ -0,0 +1,29 @@
+﻿using AliasPro.API.Network.Events;
+using AliasPro.Network.Events.Headers;
+using AliasPro.Network.Protocol;
+
+namespace AliasPro.Players.Packets.Composers
+{
+    public class UserPointsComposer : IPacketComposer
+    {
+        private readonly int _amount;
+        private readonly int _amountAdded;
+        private readonly int _type;
+
+        public UserPointsComposer(int amount, int amountAdded, int type)
+        {
+            _amount = amount;
+            _amountAdded = amountAdded;
+            _type = type;
+        }
+
+        public ServerPacket Compose()
+        {
+            ServerPacket message = new ServerPacket(Outgoing.UserPointsMessageComposer);
+            message.WriteInt(_amount);
+            message.WriteInt(_amountAdded);
+            message.WriteInt(_type);
+            return message;
+        }
+    }
+}
