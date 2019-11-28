4308dee059d18c40a9ba12dce32324778739a92f
Damien <DamienJolly@hotmail.com>
2019-11-28 12:13:49 +0000

    Added task handler.

---
+++ AliasPro/Tasks/TaskManager.cs
@@ -0,0 +1,21 @@
+﻿using AliasPro.API.Tasks;
+using System;
+using System.Threading.Tasks;
+
+namespace AliasPro.Tasks
+{
+	sealed class TaskManager
+	{
+		public static Task ExecuteTask(ITask executeTask, int delay = 0)
+		{
+			if (delay == 0)
+				return Task.Run(() => executeTask.Run());
+
+			return Task.Run(async () =>
+			{
+				await Task.Delay(TimeSpan.FromMilliseconds(delay));
+				executeTask.Run();
+			});
+		}
+	}
+}
