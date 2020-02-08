f1c0e03cfa8874b3b29ea50a6b11b42a8e5b2452
Damien <DamienJolly@hotmail.com>
2020-02-08 20:06:46 +0000

    Removed handled packet log.

--- AliasPro/Communication/Messages/MessageHandler.cs
+++ AliasPro/Communication/Messages/MessageHandler.cs
@@ -13,21 +13,20 @@ namespace AliasPro.Communication.Messages
         private readonly Dictionary<short, IMessageEvent> messageEvents;
 
         public MessageHandler(
-            ILogger<MessageHandler> logger,
+            ILogger<MessageHandler> logger, 
             IEnumerable<IMessageEvent> messageEvents)
         {
             this.logger = logger;
 
-            this.messageEvents = messageEvents.ToDictionary(x => x.Header, x => x);
+            this.messageEvents = messageEvents.ToDictionary(messageEvent => messageEvent.Header);
 
-            this.logger.LogDebug("Loaded {0} message events", this.messageEvents.Count);
+            logger.LogDebug("Loaded {0} message events", this.messageEvents.Count);
         }
 
         public async Task TryHandleAsync(ISession session, ClientMessage message)
         {
             if (messageEvents.TryGetValue(message.Id, out IMessageEvent messageEvent))
             {
-                logger.LogInformation("Handling header {0}", message.Id);
                 await messageEvent.RunAsync(session, message);
             }
             else
